9edd6c98ac831e1caa2c99b6aed97a4cc5f0aab2
Raymond Gorroh <145275681+Demagnate@users.noreply.github.com>
2025-11-09 12:58:57 +0100

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -48,9 +48,10 @@ def predict():
     return render_template("result.html", price=result)
 
 
-if ___name__ == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0" , port=port)
+
 
 
 
